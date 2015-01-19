489fbf8539d7a65b519303497b337d2ade41d1d5
rememberlenny <rememberlenny@gmail.com>
2015-01-19 11:07:09 -0500

    Create api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'status'
+  get 'api/v1/status' => 'api#status'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
