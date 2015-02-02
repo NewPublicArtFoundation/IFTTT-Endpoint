e7025981c339264b4b85493b1569b0fde7e058c1
rememberlenny <rememberlenny@gmail.com>
2015-02-02 13:22:47 -0500

    Verify for the status check

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   def status
-    if request.headers["Content-Type"] == ENV['IFTTT_Channel_Key']
+    if request.headers['IFTTT-Channel-Key'] == ENV['IFTTT_Channel_Key']
       render text: '', status: 200
     else
       render text: '', status: 401
