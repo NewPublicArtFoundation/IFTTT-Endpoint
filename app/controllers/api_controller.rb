3ab1332af693db43ec892af5a75ab2ca0dd70f10
rememberlenny <rememberlenny@gmail.com>
2015-02-02 18:25:21 -0500

    Create the item based on item

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -19,7 +19,31 @@ class ApiController < ApplicationController
     render json: @data, status: 200
   end
 
+  def get_new_pieces_in_area_item
+    item = {
+      public_art: image_url,
+      location: location_url,
+      meta: {
+        id: '',
+        timestamp: Time.now.iso8601
+      }
+    }
+    return item
+  end
+
   def triggers_new_piece_in_area
+    image_url = ''
+    location_url = ''
+
+    item = get_new_pieces_in_area_item
+
+    items = [ item ]
+
+    @data = {
+      data: items
+    }
+
+    render json: @data, status: 200
   end
 
   def setup
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   match 'api/v1/ifttt/v1/test/setup',  to: 'api#test_setup', via: [:post, :get]
 
   # Trigger
-  match 'api/v1/ifttt/v1/triggers/new_piece_in_area',  to: 'api#triggers_new_piece_in_area', via: [:get]
+  match 'api/v1/ifttt/v1/triggers/new_piece_in_area',  to: 'api#triggers_new_piece_in_area', via: [:post]
 
 end
