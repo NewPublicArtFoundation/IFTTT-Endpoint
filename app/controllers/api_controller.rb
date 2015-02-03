66fa74f282fc32b712ab85035002b88e1e603dab
rememberlenny <rememberlenny@gmail.com>
2015-02-02 20:16:10 -0500

    Prevent multiple cases from running

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -41,26 +41,24 @@ class ApiController < ApplicationController
 
     if params[:triggerFields].nil?
       render json: {errors: ['Does not have TriggerFields']}, status: 400
-    end
-
-    if params[:triggerFields][:location].nil?
+    elsif params[:triggerFields][:location].nil?
       render json: {errors: ['Does not have Location']}, status: 400
+    else
+      location = params[:triggerFields][:location]
+      limit = params[:limit]
+
+      data << get_new_pieces_in_area_items(location, id)
+      data << get_new_pieces_in_area_items(location, id)
+      data << get_new_pieces_in_area_items(location, id)
+
+      if !limit.nil?
+        data = data.slice(0, limit.to_i)
+      end
+
+      @data = { data: data }
+
+      render json: @data, status: 200
     end
-
-    location = params[:triggerFields][:location]
-    limit = params[:limit]
-
-    data << get_new_pieces_in_area_items(location, id)
-    data << get_new_pieces_in_area_items(location, id)
-    data << get_new_pieces_in_area_items(location, id)
-
-    if !limit.nil?
-      data = data.slice(0, limit.to_i)
-    end
-
-    @data = { data: data }
-
-    render json: @data, status: 200
   end
 
   def setup
